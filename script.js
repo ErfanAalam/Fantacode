19f9634c2d678ac84ca63be6357ad2c41bd9aa3a
ErfanAalam <efankhansiwani@gamil.com>
2024-03-10 22:02:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+// var tl = gsap.timelineundefined{scrollTrigger:{
+//     trigger: two
+//     start: 0 ,95,
+//     end: "70% 50%",
+//     scrub: true,
+//     // markers: true,
+// }}
+
+// tl.toundefined"#fanta",{
+//     top: "120%",
+//     left: "0%"
+// }, 'orange')
+// tl.toundefined"#orange-cut",{
+//     top:"160%",
+//     left: "23%"
+// }, 'orange')
+// tl.toundefined"#orange",{
+//     width: "15%",
+//     top:"160%",
+//     right: "10%"
+// }, 'orange')
+// tl.toundefined"#leaf",{
+//     top:"110%",
+//     rotate: "130deg",
+//     left: "70%"
+// }, 'orange')
+// tl.toundefined"#leaf2",{
+//     top:"110%",
+//     rotate: "130deg",
+//     left: "0%"
+// }, 'orange')
+
+
+// var tl2 = gsap.timelineundefined{scrollTrigger:{
+//     trigger: ".three",
+//     start: "0% 95%",
+//     end: "20% 50%",
+//     scrub: true,
+//     // markers: true,
+// }})
+
+// tl2.fromundefined".lemon1",{
+//     rotate: "-90deg",
+//     left: "-100%",
+//     top: "110%"
+// }, 'ca')
+// tl2.fromundefined"#cocacola",{
+//     rotate: "-90deg",
+//     top: "110%",
+//     left: "-100%",
+// }, 'ca')
+
+// tl2.fromundefined".lemon2",{
+//     rotate: "90deg",
+//     left: "100%",
+//     top: "110%"
+// }, 'ca')
+// tl2.fromundefined"#pepsi",{
+//     rotate: "90deg",
+//     top: "110%",
+//     left: "100%",
+// }, 'ca')
+
+// tl2.toundefined"#orange-cut",{
+//     width:"18%",
+//     left: "42%",
+//     top: "204%"
+// }, 'ca')
+// tl2.toundefined"#fanta",{
+//     width:"35%",
+//     top: "210%",
+//     left: "33%",
+// }, 'ca')
